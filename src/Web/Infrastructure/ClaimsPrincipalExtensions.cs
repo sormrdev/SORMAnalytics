c521ea9242241bd2a879225a022eb4b582fc867f
sormr <sormrdev@gmail.com>
2026-04-22 18:21:55 +0200

    fixed jwt related bug

--- src/Web/Infrastructure/ClaimsPrincipalExtensions.cs
+++ src/Web/Infrastructure/ClaimsPrincipalExtensions.cs
@@ -6,7 +6,8 @@ public static class ClaimsPrincipalExtensions
 {
     public static string? GetIdentityId(this ClaimsPrincipal? claimsPrincipal)
     {
-        string? identityId = claimsPrincipal?.FindFirstValue(ClaimTypes.NameIdentifier);
+        string? identityId = claimsPrincipal?.FindFirstValue("sub")
+            ?? claimsPrincipal?.FindFirstValue(ClaimTypes.NameIdentifier);
 
         return identityId;
     }
